5c79278a16cf67f3d5f98329f2d5ffabb98b0c72
Yana Didun <yanadidun@gmail.com>
2026-09-21 19:43:06 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t yanadidun/demo-app:jma-2.0 .'
+        sh 'docker build -t yanadidun/demo-app:jma-3.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push yanadidun/demo-app:jma-2.0'
+        sh 'docker push yanadidun/demo-app:jma-3.0'
     }
 } 
 
